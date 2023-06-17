0db5d645d4de5de80620a1b829040462920e926a
Guilherme Bley <guilhermecbley@gmail.com>
2023-06-16 23:36:11 -0300

    add fk sql

--- bd/create-db-script.sql
+++ bd/create-db-script.sql
@@ -5,8 +5,11 @@ CREATE TABLE `sales` (
    `Description` varchar(45) NOT NULL,
    `Value` double NOT NULL,
    `Date` datetime NOT NULL,
-   PRIMARY KEY (`Id`)
- ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+   `CustomerId` char(36) NOT NULL,
+   PRIMARY KEY (`Id`),
+   KEY `fk_sales_customer_idx` (`CustomerId`),
+   CONSTRAINT `fk_sales_customer` FOREIGN KEY (`CustomerId`) REFERENCES `customer` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+ ) ENGINE=InnoDB DEFAULT CHARSET=latin1
 
  CREATE TABLE `customer` (
    `id` char(36) NOT NULL,
